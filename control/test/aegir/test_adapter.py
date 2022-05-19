7762a002ba84790a79192eb37aeb974a5f7cae85
James Foster <bdf69512@te7aegdev06.te.rl.ac.uk>
2022-05-19 15:05:21 +0100

    Updated the commenting and formatting of the unit test code.

--- control/test/aegir/test_adapter.py
+++ control/test/aegir/test_adapter.py
@@ -1,14 +1,10 @@
-from ast import Import
+"""Test adapter class.
+
+James Foster
+"""
 import pytest
 import sys
 from aegir.adapter import AegirAdapter
-from aegir.controller import AegirControllerError
-
-from odin.adapters.dummy import DummyAdapter
-from odin.adapters.parameter_tree import ParameterAccessor, ParameterTree, ParameterTreeError
-from odin.adapters.adapter import ApiAdapter, ApiAdapterRequest, ApiAdapterResponse, request_types, response_types
-from odin._version import get_versions
-from copy import deepcopy
 
 if sys.version_info[0] == 3:  # pragma: no cover
     from unittest.mock import Mock
@@ -16,111 +12,36 @@ if sys.version_info[0] == 3:  # pragma: no cover
 else:                         # pragma: no cover
     from mock import Mock
 
-class DummyAdapterTestFixture(object):
-    """Container class used in fixtures for testing the DummyAdapter."""
-
-    def __init__(self):
-
-        self.adapter = DummyAdapter(background_task_enable=True)
-        self.path = '/dummy/path'
-        self.request = Mock()
-        self.request.headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
-
-@pytest.fixture(scope="class")
-def test_dummy_adapter():
-    """Simple test fixture for testing the dummy adapter."""
-    test_dummy_adapter = DummyAdapterTestFixture()
-    yield test_dummy_adapter
 
 @pytest.fixture(scope="class")
 def dummy_aegir_adapter():
+    """Test the simple test fixture used in testing the aegir adapter."""
     dummy_aegir_adapter = AegirAdapter()
     yield dummy_aegir_adapter
 
+
 class TestAegirAdapter():
+    """Class for testing aegir adapter behaviour."""
+
     def test_adapter_get(self, dummy_aegir_adapter):
+        """Test the adapter get method returns the appropriate response."""
         expected_response = {'status': 'unknown'}
         mock_request = Mock()
         mock_request.headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
         response = dummy_aegir_adapter.get('status', mock_request)
         assert response.data == expected_response
         assert response.status_code == 200
-    
+
     def test_adapter_get_bad_param_tree(self, dummy_aegir_adapter):
+        """Test that attempting to get an inappropriate value returns an error."""
         expected_response = {'error': 'Invalid path: bad_param_tree'}
         mock_request = Mock()
         mock_request.headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
         response = dummy_aegir_adapter.get('bad_param_tree', mock_request)
         assert response.data == expected_response
         assert response.status_code == 400
-    
+
     def test_cleanup(self, dummy_aegir_adapter):
+        """Test the cleanup function."""
         dummy_aegir_adapter.cleanup()
-        assert dummy_aegir_adapter.controller.background_task_enable == False
-
-    
-
-class TestDummyAdapter():
-
-    def test_dummy(self):
-        '''
-        Dummy test to verify pytest setup
-        '''
-        assert True
-
-    def test_adapter_get2(self, test_dummy_adapter):
-        """Test that a call to the GET method of the dummy adapter returns the correct response."""
-        expected_response = {
-            'response': 'DummyAdapter: GET on path {}'.format(test_dummy_adapter.path)
-            }
-        response = test_dummy_adapter.adapter.get(test_dummy_adapter.path, 
-            test_dummy_adapter.request)
-        assert response.data == expected_response
-        assert response.status_code == 200
-
-    def test_adapter_put(self, test_dummy_adapter):
-        """Test that a call to the PUT method of the dummy adapter returns the correct response."""
-        expected_response = {
-            'response': 'DummyAdapter: PUT on path {}'.format(test_dummy_adapter.path)
-            }
-        response = test_dummy_adapter.adapter.put(test_dummy_adapter.path,
-            test_dummy_adapter.request)
-        assert response.data == expected_response
-        assert response.status_code == 200
-
-    def test_adapter_delete(self, test_dummy_adapter):
-        """Test that a call to the DELETE method of the dummy adapter returns the correct response."""
-        response = test_dummy_adapter.adapter.delete(test_dummy_adapter.path, 
-            test_dummy_adapter.request)
-        assert response.data == 'DummyAdapter: DELETE on path {}'.format(test_dummy_adapter.path)
-        assert response.status_code == 200
-
-    def test_adapter_put_bad_content_type(self, test_dummy_adapter):
-        """
-        Test that a call to the dummy adapter with an incorrect content type generates the
-        appropriate error response.
-        """
-        bad_request = Mock()
-        bad_request.headers = {'Content-Type': 'text/plain'}
-        response = test_dummy_adapter.adapter.put(test_dummy_adapter.path, bad_request)
-        assert response.data == 'Request content type (text/plain) not supported'
-        assert response.status_code == 415
-
-    def test_adapter_put_bad_accept_type(self, test_dummy_adapter):
-        """
-        Test that a call to the dummy adapter with an incorrect accept type generates the
-        appropriate error response.
-        """
-        bad_request = Mock()
-        bad_request.headers = {'Accept': 'text/plain'}
-        response = test_dummy_adapter.adapter.put(test_dummy_adapter.path, bad_request)
-        assert response.data == 'Requested content types not supported'
-        assert response.status_code == 406
-
-    def test_adapter_cleanup(self, test_dummy_adapter):
-        """
-        Test that a call the dummy adapter cleanup method cleans up correctly.
-        """
-        test_dummy_adapter.adapter.background_task_counter = 1000
-        test_dummy_adapter.adapter.cleanup()
-        assert test_dummy_adapter.adapter.background_task_counter == 0
+        assert not dummy_aegir_adapter.controller.background_task_enable
--- control/test/aegir/test_packet_decoder.py
+++ control/test/aegir/test_packet_decoder.py
@@ -1,24 +1,17 @@
-from ast import Import
+"""Test packet decoder class.
+
+James Foster
+"""
 import pytest
-from aegir.controller import AegirController, AegirControllerError
 from aegir.packet_decoder import AegirPacketDecoder
-from datetime import datetime
 import struct
-import os, serial
 
-from odin.adapters.dummy import DummyAdapter
-from odin.adapters.parameter_tree import ParameterAccessor, ParameterTree, ParameterTreeError
-from odin.adapters.adapter import ApiAdapter, ApiAdapterRequest, ApiAdapterResponse, request_types, response_types
-from odin._version import get_versions
-from copy import deepcopy
-
-try:
-    import pty
-except ImportError:
-    pty = None
 
 class DecoderTestFixture(object):
+    """Container class used in the creation of a packet decoder fixture."""
+
     def __init__(self):
+        """Initialise the packet decoder and test packets."""
         self.good_packet = struct.pack('<HH?BH', 1, 2, 0, 3, 42405)
         self.bad_checksum = struct.pack('<HH?BH', 1, 2, 0, 0, 42405)
         self.bad_eop = struct.pack('<HH?BH', 1, 2, 0, 3, 0)
@@ -26,39 +19,49 @@ class DecoderTestFixture(object):
 
         self.decoder = AegirPacketDecoder()
 
+
 @pytest.fixture(scope="class")
 def decoder_fixture():
+    """Test fixture used in the testing of packet decoder behaviour."""
     decoder_fixture = DecoderTestFixture()
     yield decoder_fixture
 
+
 class TestPacketDecoder():
+    """Class to test the packet decoder behaviour."""
 
     def test_unpack(self, decoder_fixture):
+        """Test that a packet is unpacked correctly."""
         decoder_fixture.decoder.unpack(decoder_fixture.good_packet)
         assert decoder_fixture.decoder.t1 == 1
         assert decoder_fixture.decoder.t2 == 2
 
     def test_packet_complete(self, decoder_fixture):
+        """Test that an appropriate packet meets the requirements of a completed packet."""
         if decoder_fixture.decoder.packet_complete(decoder_fixture.good_packet):
             packet_completed = True
-        assert packet_completed == True
+        assert packet_completed
 
     def test_packet_complete_bad_eop(self, decoder_fixture):
+        """Test that an inappropriate packet does not meet the requirements of a complete packet."""
         if not decoder_fixture.decoder.packet_complete(decoder_fixture.bad_eop):
             packet_completed = False
-        assert packet_completed == False
+        assert not packet_completed
 
     def test_checksum(self, decoder_fixture):
+        """Test that the checksum validation method validates an appropriate packet."""
         decoder_fixture.decoder.unpack(decoder_fixture.good_packet)
         decoder_fixture.decoder.checkSumCheck(decoder_fixture.good_packet)
-        assert decoder_fixture.decoder.csumValid == True
-    
+        assert decoder_fixture.decoder.csumValid
+
     def test_checksum_bad(self, decoder_fixture):
+        """Test that the checksum validation method does not validate an inappropriate packet."""
         decoder_fixture.decoder.unpack(decoder_fixture.bad_checksum)
         decoder_fixture.decoder.checkSumCheck(decoder_fixture.bad_checksum)
-        assert decoder_fixture.decoder.csumValid == False
+        assert not decoder_fixture.decoder.csumValid
 
     def test_make_dict(self, decoder_fixture):
+        """Test that an unpacked packet is returned as a dictionary using the method."""
         decoder_fixture.decoder.unpack(decoder_fixture.good_packet)
         decoder_dict = decoder_fixture.decoder.make_dict()
 
@@ -66,11 +69,13 @@ class TestPacketDecoder():
         assert decoder_dict["t2"] == 2
 
     def test_string_format(self, decoder_fixture):
+        """Test that an unpacked packet is returned as a string."""
         decoder_fixture.decoder.unpack(decoder_fixture.good_packet)
         decoder_string = str(decoder_fixture.decoder)
 
         assert decoder_string == "t1=1 t2=2 fault=False checksum=3 eop=0xa5a5"
-    
+
     def test_large_packet_size(self, decoder_fixture):
+        """Test that a packet which is too large raises an error."""
         with pytest.raises(struct.error):
             decoder_fixture.decoder.unpack(decoder_fixture.bad_large_packet)
