63f34b57ea87f1d54d68ba05132f49ac87eab7c4
James Foster <james.foster@stfc.ac.uk>
2022-06-30 14:11:38 +0100

    Updated web UI layout, added speed toggle button for charts, added alerts for failed packets and fault detections

--- control/test/static/js/aegir.js
+++ control/test/static/js/aegir.js
@@ -11,49 +11,35 @@ document.addEventListener("DOMContentLoaded", function () {
     // Initialise the adapter endpoint
     aegir_endpoint = new AdapterEndpoint("aegir");
     init();
-
-    update_api_version();
-    update_api_info();
     poll_update();
-
     poll_chart_update();
 });
 
 function init()
 {
+    // Initialise the charts
     init_environment_tracking();
-}
-
-function update_api_version()
-{
-    document.querySelector('#api-version').innerHTML = aegir_endpoint.api_version;
-}
-
-function update_api_info()
-{
-    aegir_endpoint.get_adapters()
-    .then(result => {
-        adapter_list = result.adapters.join(", ")
-        document.querySelector('#api-adapters').innerHTML = adapter_list;
-    })
-    .catch(error => {
-        console.log(error.message);
-    });
 }
 
 function poll_update() 
 {
+    // Update the data on the page
     update_background_task();
+    update_failed_packet_notif();
+    update_fault_det_notif();
     setTimeout(poll_update, 500);
 }
 
 function poll_chart_update()
 {
+    // Update the chart information
     update_chart_temps();
-    setTimeout(poll_chart_update, 4000);
+    setTimeout(poll_chart_update, get_chart_update_speed());
 }
 
-function set_chart_animation_duration(duration) {
+function set_chart_animation_duration(duration) 
+{
+    // A cookie which sets the speed of chart animation.
     chart_temp.options.animation.duration = duration
     chart_hum.options.animation.duration = duration
 
@@ -64,7 +50,9 @@ function set_chart_animation_duration(duration) {
     document.cookie = "chartAnimationDuration="+duration.toString()+";"+expires+";path=/;SameSite=Strict;";
 }
 
-function get_chart_animation_duration() {
+function get_chart_animation_duration() 
+{
+    // Retrieves the chart animation duration cookie.
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
     let name = "chartAnimationDuration=";
@@ -80,9 +68,52 @@ function get_chart_animation_duration() {
     return 1000;  // Default duration if no cookie is found
 }
 
+function set_chart_update_speed(speed) 
+{
+    // A cookie which sets the speed of chart update.
+    const d = new Date();
+    exdays = 30;    // Keep this cookie for 30 days
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = "chartUpdateSpeed="+speed.toString()+";"+expires+";path=/;SameSite=Strict;";
+}
+
+function get_chart_update_speed()
+{
+    // Retrieves the chart update speed cookie.
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    let name = "chartUpdateSpeed=";
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+		while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return parseInt(c.substring(name.length, c.length));
+        }
+    }
+    return 4000;  // Default duration if no cookie is found
+}
+
+function toggle_chart_speed()
+{
+    // Toggles chart speed between fast and slow.
+    var current_speed = get_chart_update_speed();
+    if (current_speed == 4000) {
+        set_chart_update_speed(2000);
+        set_chart_animation_duration(500);
+    } else {
+        set_chart_update_speed(4000);
+        set_chart_animation_duration(1000);
+    }
+}
+
 var chart_temp;
 var chart_hum;
-function init_environment_tracking() {
+function init_environment_tracking() 
+{
+    // Builds the charts with the relevant information.
     var chart_element = document.getElementById("temperature-chart").getContext('2d');
     chart_temp = new Chart(chart_element,
     {
@@ -107,15 +138,22 @@ function init_environment_tracking() {
                     tension: 0
                     }
             },
-	    scales: {
-	        xAxes: [{
+            scales: {
+                xAxes: [{
                     type: 'time',
-		    distribution: 'linear',
-		    ticks: {
-	                source: 'data',
-		    }
-		}]
-	    }
+                    distribution: 'linear',
+                    ticks: {
+                        source: 'data',
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        min: -5,
+                        max: 50,
+                        stepSize: 5,
+                    }
+                }]
+            }
         }
     });
 
@@ -144,18 +182,26 @@ function init_environment_tracking() {
                     tension: 0
                     }
             },
-	    scales: {
-	        xAxes: [{
+            scales: {
+                xAxes: [{
                     type: 'time',
-            animation: {
-                duration: get_chart_animation_duration(),
-            },
-		    distribution: 'linear',
-			ticks: {
-	                source: 'data',
-		    }
-		}]
-	    }
+                    animation: {
+                        duration: get_chart_animation_duration(),
+                    },
+                    distribution: 'linear',
+                    ticks: {
+                        source: 'data',
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        min: 0,
+                        max: 100,
+                        stepSize: 10,
+                    }
+                }]
+            }
         }
     });
 }
@@ -166,8 +212,9 @@ var environment_tracking_time = [];
 var environment_tracking_valuelimit = 40;
 var temp_ambient = NaN;
 var hum_ambient = NaN;
-function update_chart_temps() {
-
+function update_chart_temps() 
+{
+    // Updates the information on the charts.
     aegir_endpoint.get('')
     .then(result => {
         temp_ambient = result.packet_info.temp.toFixed(2);
@@ -195,20 +242,23 @@ function update_chart_temps() {
     chart_hum.update();
 }
 
+var task_bad_packets = NaN
+var task_packet_fault = NaN
 function update_background_task()
 {
+    // Updates the information for the text boxes.
     aegir_endpoint.get('')
     .then(result => {
         var task_status = result.status;
 
-        var task_packet_temp = result.packet_info.temp;
-        var task_packet_humidity = result.packet_info.humidity;
-        var task_packet_fault = result.packet_info.fault;
-        var task_packet_checksum = result.packet_info.checksum;
-        var task_packet_eop = result.packet_info.eop;
+        temp_ambient = result.packet_info.temp;
+        hum_ambient = result.packet_info.humidity;
+        task_packet_fault = result.packet_info.fault;
+        // var task_packet_checksum = result.packet_info.checksum;
+        // var task_packet_eop = result.packet_info.eop;
 
         var task_good_packets = result.good_packets;
-        var task_bad_packets = result.bad_packets;
+        task_bad_packets = result.bad_packets;
 
         var task_time_received = result.time_received;
 
@@ -216,15 +266,15 @@ function update_background_task()
             `${task_status}`;
 
         document.querySelector("#packet-temp").innerHTML = 
-            `${task_packet_temp.toFixed(2)}`;
+            `${temp_ambient.toFixed(2)}`;
         document.querySelector("#packet-humidity").innerHTML = 
-            `${task_packet_humidity.toFixed(2)}`;
+            `${hum_ambient.toFixed(2)}`;
         document.querySelector("#packet-fault").innerHTML = 
             `${task_packet_fault}`;
-        document.querySelector("#packet-checksum").innerHTML = 
-            `${task_packet_checksum}`;
-        document.querySelector("#packet-eop").innerHTML = 
-            `${task_packet_eop}`;
+        // document.querySelector("#packet-checksum").innerHTML = 
+        //     `${task_packet_checksum}`;
+        // document.querySelector("#packet-eop").innerHTML = 
+        //     `${task_packet_eop}`;
 
         document.querySelector("#good-packets").innerHTML = 
             `${task_good_packets}`;
@@ -237,4 +287,34 @@ function update_background_task()
     .catch(error => {
         console.log(error.message);
     });
+}
+
+var fail_packet_alert_shown = false;
+function update_failed_packet_notif()
+{
+    // Displays an alert if a packet has failed.
+    if (task_bad_packets > 0) {
+        if(!fail_packet_alert_shown) {
+            $('#failed-packet').fadeIn();
+            fail_packet_alert_shown = true;
+        }
+    } else {
+        $('#failed-packet').hide();
+        fail_packet_alert_shown = false;
+    }
+}
+
+var fault_det_alert_shown = false;
+function update_fault_det_notif()
+{
+    // Displays an alert if a fault is detected.
+    if (task_packet_fault) {
+        if(!fault_det_alert_shown) {
+            $('#fault-detected').fadeIn();
+            fault_det_alert_shown = true;
+        }
+    } else {
+        $('#fault-detected').hide();
+        fault_det_alert_shown = false;
+    }
 }
