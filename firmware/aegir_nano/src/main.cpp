40d66caaec22c1cf2a71462b76707ee858a9698c
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2023-03-08 10:30:20 +0000

    Update code to fully support v2 hardware devices

--- firmware/aegir_nano/src/main.cpp
+++ firmware/aegir_nano/src/main.cpp
@@ -12,11 +12,6 @@
 
 #include "AnalogueThreshold.h"
 
-// #include <Wire.h>
-
-// #include "AD7994.h"
-// #include "SHT31.h"
-// #include "PT100.h"
 #include "AegirData.h"
 
 // Pin definitions
@@ -24,8 +19,9 @@
 #define LEAK_DETECT_PIN 3
 #define WARNING_CONDITION_PIN 4
 #define ERROR_CONDITION_PIN 6
-#define PT100_T1_CS_PIN 7
-#define PT100_T2_CS_PIN 8
+#define GPIO_OUTPUT_PIN 5
+#define PT100_T1_CS_PIN 8
+#define PT100_T2_CS_PIN 7
 #define RS485_DE_PIN 9
 #define RS485_RE_PIN 10
 
@@ -37,19 +33,31 @@
 #define DEBUG_PRINT 1
 
 // Devices and data structure instances
-// AD7994 ad7994;
-// SHT31 sht31;
-// PT100 pt100;
 Adafruit_BME280 bme280;
 Adafruit_MAX31865 pt100[] = {
     Adafruit_MAX31865(PT100_T1_CS_PIN),
     Adafruit_MAX31865(PT100_T2_CS_PIN)
 };
-AegirData tx_data;
-
-AnalogueThreshold threshold1(PIN_A0, 0.0, 100.0);
 
 const unsigned int num_pt100 = sizeof(pt100) / sizeof(pt100[0]);
+
+enum Threshold
+{
+    board_temp = 0,
+    board_humidity = 1,
+    probe_temp_1 = 2,
+    probe_temp_2 = 3,
+};
+
+AnalogueThreshold threshold[] = {
+    AnalogueThreshold("board_temp", PIN_A3, 0.0, 100.0, 1.0),
+    AnalogueThreshold("board_humidity", PIN_A2, 0.0, 100.0, 1.0),
+    AnalogueThreshold("probe_temp_1", PIN_A1, 0.0, 100.0, 1.0),
+    AnalogueThreshold("probe_temp_2", PIN_A0, 0.0, 100.0, 1.0),
+};
+const unsigned int num_threshold = sizeof(threshold) / sizeof(threshold[0]);
+
+AegirData tx_data;
 
 // Forward declarations
 void dump_data(void);
@@ -68,27 +76,21 @@ void setup()
     while (!Serial1) { ;}
 
     // Set GPIO pins to appropriate modes
-    // pinMode(LED_BUILTIN, OUTPUT);
-    // pinMode(GPIO_OUTPUT_PIN, OUTPUT);
     pinMode(LEAK_CONTINUITY_PIN, INPUT);
     pinMode(LEAK_DETECT_PIN, INPUT);
     pinMode(WARNING_CONDITION_PIN, OUTPUT);
     pinMode(ERROR_CONDITION_PIN, OUTPUT);
+    pinMode(GPIO_OUTPUT_PIN, OUTPUT);
 
     digitalWrite(WARNING_CONDITION_PIN, LOW);
     digitalWrite(ERROR_CONDITION_PIN, LOW);
+    digitalWrite(GPIO_OUTPUT_PIN, LOW);
 
     // Set RS485 transceiver RE and DE pins to output and set high to enable transmission
     pinMode(RS485_DE_PIN, OUTPUT);
     pinMode(RS485_RE_PIN, OUTPUT);
     digitalWrite(RS485_DE_PIN, HIGH);
     digitalWrite(RS485_RE_PIN, HIGH);
-
-    // // Initialise the I2C bus
-    // Wire.begin();
-
-    // // Initialise the ADC
-    // ad7994.begin();
 
     // Initialise the BME280 sensor
     bool status = bme280.begin();
@@ -99,7 +101,8 @@ void setup()
         Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
         Serial.print("        ID of 0x60 represents a BME 280.\n");
         Serial.print("        ID of 0x61 represents a BME 680.\n");
-        while (1) delay(10);
+
+        // TODO set error bit in status word
     }
 
     // Initialise the PT100 sensors (MAX31865 devices)
@@ -108,13 +111,7 @@ void setup()
     for (int idx = 0; idx < num_pt100; idx++)
     {
         pt100[idx].begin(MAX31865_4WIRE);
-    }
-
-    for (int idx = 0; idx < 5; idx++)
-    {
-        threshold1.update();
-        float value = threshold1.value();
-        Serial.println("Calculated value is: " + String(value));
+        pt100[idx].enable50Hz(true);
     }
 
 }
@@ -124,52 +121,54 @@ void setup()
 void loop()
 {
 
-    // Static led state flag - used to bcycle LED to indicate activity
-    static uint8_t led_state = HIGH;
-
-    // Toggle state of LED
-    led_state = (led_state == HIGH ? LOW : HIGH);
-    // digitalWrite(LED_BUILTIN, led_state);
-
     // Set GPIO output pin high - this is done temporarily to time the loop functionality
-    // digitalWrite(GPIO_OUTPUT_PIN, HIGH);
+    digitalWrite(GPIO_OUTPUT_PIN, HIGH);
 
-    // Read the GPIO pins for leak continuity, detection and overall fault condition
+    // Read the GPIO pins for leak continuity and detection
     tx_data.leak_continuity = digitalRead(LEAK_CONTINUITY_PIN);
-    // tx_data.leak_detected = 1 - digitalRead(LEAK_DETECT_PIN);
     tx_data.leak_detected = digitalRead(LEAK_DETECT_PIN);
-    // tx_data.fault_condition = 1 - digitalRead(FAULT_CONDITION_PIN);
     tx_data.fault_condition = 0;
+    tx_data.warning_condition = 0;
 
-    // digitalWrite(ERROR_CONDITION_PIN, led_state);
-
-    // Read the raw ADC values
-    for (uint8_t idx = 0; idx < AEGIR_ADC_CHANNELS; idx++)
+    // Update the analogue thresholds
+    for (uint8_t idx = 0; idx < num_threshold; idx++)
     {
-        // tx_data.adc_val[idx] = ad7994.read_adc_chan(idx);
-        tx_data.adc_val[idx] = analogRead(A0 + idx);
+        threshold[idx].update();
+        tx_data.threshold[idx] = threshold[idx].value();
     }
 
-    // Convert the raw ADC values into the appropriate measurements
-    // tx_data.board_humidity = sht31.relative_humidity((float)tx_data.adc_val[0] / 4095.0);
-    // tx_data.board_temperature = sht31.temperature((float)tx_data.adc_val[1] / 4095.0);
-    // tx_data.probe_temperature[0] = pt100.temperature(tx_data.adc_val[2]);
-    // tx_data.probe_temperature[1] = pt100.temperature(tx_data.adc_val[3]);
-    tx_data.board_humidity = bme280.readHumidity();
+    // Update all sensor measurements
     tx_data.board_temperature = bme280.readTemperature();
+    tx_data.board_humidity = bme280.readHumidity();
     for (int idx = 0; idx < num_pt100; idx++)
     {
         tx_data.probe_temperature[idx] = pt100[idx].temperature(RNOMINAL, RREF);
     }
 
+    // Compare the sensor readings with their respective thresholds
+    bool board_temp_ok = threshold[Threshold::board_temp].compare(tx_data.board_temperature);
+    bool board_humidity_ok = threshold[Threshold::board_humidity].compare(tx_data.board_humidity);
+    bool probe_temp_0_ok = threshold[Threshold::probe_temp_1].compare(tx_data.probe_temperature[0]);
+    bool probe_temp_1_ok = threshold[Threshold::probe_temp_2].compare(tx_data.probe_temperature[1]);
+
+    // Evaluate the warning condition based on board temperature and humidity and update warning
+    // pin state accordingly
+    tx_data.warning_condition = !(board_temp_ok && board_humidity_ok);
+    digitalWrite(WARNING_CONDITION_PIN, tx_data.warning_condition);
+
+    // Evaluate the error condition based on probe temperatures and leak continuity and update
+    // error pin state accordingly
+    bool error_condition = !(
+        tx_data.leak_continuity && probe_temp_0_ok && probe_temp_1_ok
+    );
+    digitalWrite(ERROR_CONDITION_PIN, error_condition);
+
+    // The fault condition output to the controller board is OR of error condition and leak
+    // detection state, so mirror that in the transmitted data structure
+    tx_data.fault_condition = tx_data.leak_detected | error_condition;
+
     // Update the data structure checksum
     tx_data.update_checksum();
-
-    // Print debug output if enabled
-    if (DEBUG_PRINT)
-    {
-        dump_data();
-    }
 
     // Transmit the data structure over the RS485 serial port
     uint8_t* ptr = (uint8_t *)&tx_data;
@@ -178,8 +177,14 @@ void loop()
         Serial1.write(ptr[idx]);
     }
 
+    // Print debug output if enabled
+    if (DEBUG_PRINT)
+    {
+        dump_data();
+    }
+
     // Toggle the GPIO pin low to signal end the loop activity
-   //  digitalWrite(GPIO_OUTPUT_PIN, LOW);
+    digitalWrite(GPIO_OUTPUT_PIN, LOW);
 
     // Delay for 1 second
     delay(1000);
@@ -198,37 +203,33 @@ void dump_data(void)
     Serial.print(tx_data.leak_continuity);
 
     Serial.print(" Fault: ");
-    Serial.println(tx_data.fault_condition);
+    Serial.print(tx_data.fault_condition);
 
-    Serial.print("Raw ADC: ");
-    for (int idx = 0; idx < 4; idx++)
+    Serial.print(" Warning: ");
+    Serial.println(tx_data.warning_condition);
+
+    Serial.print("Thresholds: ");
+    for (int idx = 0; idx < num_threshold; idx++)
     {
-        Serial.print(idx);
-        Serial.print(": 0x");
-        Serial.print(tx_data.adc_val[idx], HEX);
-        Serial.print(" (");
-        Serial.print(tx_data.adc_val[idx]);
-        Serial.print(", ");
-        // Serial.print(ad7994.adc_to_volts(tx_data.adc_val[idx]), 2);
-        Serial.print(((float)tx_data.adc_val[idx] / 1023) * 5.0, 2);
-        Serial.print("V) ");
+        Serial.print(String(idx) + ": " + threshold[idx].name() + " ");
+        Serial.print(tx_data.threshold[idx], 1);
+        Serial.print(" ");
     }
     Serial.println("");
 
-    Serial.print("SHT31: rel humidity: ");
-    Serial.print(tx_data.board_humidity, 1);
-    Serial.print(" % ");
-    Serial.print("temp: ");
+    Serial.print("Board: temp ");
     Serial.print(tx_data.board_temperature, 1);
-    Serial.println(" C");
+    Serial.print(" C rel humidity: ");
+    Serial.print(tx_data.board_humidity, 1);
+    Serial.println(" % ");
 
-    Serial.print("PT100 temps: 1: ");
+    Serial.print("Probe temps: 1: ");
     Serial.print(tx_data.probe_temperature[0], 1);
-    Serial.print(" (+/-)0.5 C ");
+    Serial.print(" C ");
 
     Serial.print("2: ");
     Serial.print(tx_data.probe_temperature[1], 1);
-    Serial.println(" (+/-)0.5 C");
+    Serial.println(" C");
 
     Serial.print("Checksum: 0x");
     Serial.print((int)tx_data.checksum, HEX);
