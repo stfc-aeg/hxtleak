8c4278d5ccbaca2a4d29c134edcac66ae280a663
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2023-03-13 08:48:29 +0000

    Tidy up excess debug output

--- firmware/aegir_nano/src/main.cpp
+++ firmware/aegir_nano/src/main.cpp
@@ -73,12 +73,13 @@ void setup()
 
     //Initialise the serial comms port
     Serial.begin(57600);
-    while (!Serial) { ;}
-    Serial.println("AEGIR startup");
+    if (DEBUG_PRINT)
+    {
+        Serial.println("AEGIR startup");
+    }
 
     // Set up the second serial port for RS485 data transmission
     Serial1.begin(57600);
-    while (!Serial1) { ;}
 
     // Set GPIO pins to appropriate modes
     pinMode(LEAK_CONTINUITY_PIN, INPUT);
@@ -99,20 +100,17 @@ void setup()
 
     // Initialise the BME280 sensor
     bool status = bme280.begin();
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); Serial.println(bme280.sensorID(),16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
-
+    if (!status)
+    {
+        if (DEBUG_PRINT)
+        {
+            Serial.print("Could not find a valid BME280 sensor, sensor ID was");
+            Serial.println(bme280.sensorID(),16);
+        }
         // TODO set error bit in status word
     }
 
     // Initialise the PT100 sensors (MAX31865 devices)
-    Serial.print("Number of PT100 channels: "); Serial.println(num_pt100);
-
     for (int idx = 0; idx < num_pt100; idx++)
     {
         pt100[idx].begin(MAX31865_4WIRE);
