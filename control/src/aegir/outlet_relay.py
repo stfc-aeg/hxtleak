5731dbdf62e62be7d79e6a5f72a7ae5d31061ad3
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2023-03-16 10:16:51 +0000

    Add logging and name to OutletRelay

--- control/src/aegir/outlet_relay.py
+++ control/src/aegir/outlet_relay.py
@@ -5,6 +5,8 @@ can be controlled, and the outlet control enabled/disabled as appropriate during
 
 Tim Nicholls, STFC Detector Systems Software Group
 """
+import logging
+
 from .gpio import Gpio
 from .util import AegirError
 
@@ -15,21 +17,39 @@ class OutletRelay():
 
     The class implmenents control of the AEGIR power outlet relays via GPIO pins.
     """
-    def __init__(self, gpio_pin, enabled=True, state=False):
+
+    logger = logging.getLogger()  # Logger for OutletRelay instances - defaults to root logger
+
+    @classmethod
+    def set_logger(cls, logger):
+        """Set the logger for OutletRelay instances.
+
+        This class method sets the logger for OutletRelay instance message logging. If not called,
+        logging will default to the root logger.
+
+        :param logger: logger instance to use
+        """
+        cls.logger = logger
+
+    def __init__(self, name, gpio_pin, enabled=True, state=False):
         """
         Initialise the outlet relay.
 
         Intialises the outlet relay on the specified GPIO pin. The initial enable and state of the
         relay can be set.
 
+        :param name: name of the outlet, used for log messages
         :param gpio_pin: GPIO pin descriptor
         :param enabled: initial enable state (default True = enabled)
         :param state: initial outlet state (default False = off)
         """
+        self.name = name
         self.gpio = Gpio(gpio_pin, Gpio.OUT)
 
         self.enabled = enabled
         self.state = state
+
+        self.logger.debug("%s outlet initialised", self.name)
 
     def set_enabled(self, enable):
         """
@@ -57,9 +77,11 @@ class OutletRelay():
         self.state = bool(state)
         self.gpio.write(self.state)
 
+        self.logger.info("%s outlet state set to %s", self.name, "on" if state else "off")
+
     def tree(self):
         """
-        Returns a dict-like tree of outlet relay parameters.
+        Return a dict-like tree of outlet relay parameters.
 
         Thie method returns a dict-like tree out outlet relay parameters, i.e. the state and enable
         values and their accessors. It is intended to be incorporated into a ParameterTree instance
